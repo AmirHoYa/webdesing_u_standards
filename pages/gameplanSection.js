3b16c80d7113cb56589ddaefa60419ee4885707b
Asmir <amir.h.yaghubi@gmail.com>
2024-08-25 15:55:32 +0200

    finished gameplanSection

--- pages/gameplanSection.js
+++ pages/gameplanSection.js
@@ -5,6 +5,7 @@ const killerContent = document.getElementById('killerContent');
 const survivorContent = document.getElementById('survivorContent');
 let isRed = false;
 let isBlue = false;
+let currentlyDisplayed = '';
 
 // Initialisiere das Canvas mit gleichmäßig verteilten Farben
 function initCanvas() {
@@ -16,18 +17,24 @@ function initCanvas() {
 
 // Setze die Canvas-Farben basierend auf der Auswahl
 function setCanvasColors() {
+    if (canvas.classList.contains('glow-blue') || canvas.classList.contains('glow-red')) {
+        canvas.classList.remove('glow-blue', 'glow-red');
+    }
+
     if (isRed) {
         // 80% Rot und 20% Blau (Rot rechts, Blau links)
         ctx.fillStyle = 'blue';
         ctx.fillRect(0, 0, canvas.width * 0.2, canvas.height);
         ctx.fillStyle = 'red';
         ctx.fillRect(canvas.width * 0.2, 0, canvas.width * 0.8, canvas.height);
+        canvas.classList.add('glow-red');
     } else if (isBlue) {
         // 80% Blau und 20% Rot (Blau links, Rot rechts)
         ctx.fillStyle = 'blue';
         ctx.fillRect(0, 0, canvas.width * 0.8, canvas.height);
         ctx.fillStyle = 'red';
         ctx.fillRect(canvas.width * 0.8, 0, canvas.width * 0.2, canvas.height);
+        canvas.classList.add('glow-blue');
     } else {
         initCanvas();
     }
@@ -36,27 +43,40 @@ function setCanvasColors() {
 // Handler für das Klicken der Bilder
 function handleImageClick(img) {
     const additionalContent = document.getElementById('additionalContent');
+
     if (img.src.includes('trapper.png')) {
-        if (isRed) {
-            isRed = false;
+        if (currentlyDisplayed === 'trapper') {
             additionalContent.style.display = 'none';
+            currentlyDisplayed = ''; // Zurücksetzen, wenn der Inhalt versteckt wird
+            isBlue = false;
+            isRed = false;
+            img.classList.remove('glow-red');
         } else {
+            currentlyDisplayed = 'trapper';
             isRed = true;
             isBlue = false;
             additionalContent.style.display = 'block';
+            img.classList.add('glow-red');
+            updateAdditionalContent(img.src);
         }
     } else if (img.src.includes('ada_wong.png')) {
-        if (isBlue) {
-            isBlue = false;
+        if (currentlyDisplayed === 'ada_wong') {
             additionalContent.style.display = 'none';
+            currentlyDisplayed = ''; // Zurücksetzen, wenn der Inhalt versteckt wird
+            isBlue = false;
+            isRed = false;
+            img.classList.remove('glow-blue');
         } else {
+            currentlyDisplayed = 'ada_wong';
             isBlue = true;
             isRed = false;
             additionalContent.style.display = 'block';
+            img.classList.add('glow-blue');
+            updateAdditionalContent(img.src);
         }
     }
+
     setCanvasColors();
-    updateAdditionalContent(img.src);
 }
 
 function updateAdditionalContent(type) {
